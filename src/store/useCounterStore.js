5288713d172dde181a8adb1f84fd611d42aca618
seonwooke <seonookkim@gmail.com>
2023-04-27 17:09:35 +0900

    :zap: React Zustand Todo App
    React Zustand Todo App

---
+++ src/store/useCounterStore.js
@@ -0,0 +1,16 @@
+import {create} from "zustand";
+import {devtools, persist} from "zustand/middleware";
+
+let counterStore = (set) => ({
+    count: 1,
+    increment: () => set((state) => ({
+        count: state.count + 1
+    })),
+    reset: () => set({count: 1}),
+    setNumber: (number) => set({count: 3})
+})
+
+counterStore = devtools(counterStore);
+counterStore = persist(counterStore, { name: 'counter' });
+
+export const useCounterStore = create(counterStore);
